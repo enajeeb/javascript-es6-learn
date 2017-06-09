0336f5dfd7c2b6910761838f8e12fab940073d29
Emad Najeeb <emad.najeeb@gmail.com>
2017-06-09 15:57:10 -0700

    updates

--- classes.js
+++ classes.js
@@ -1,9 +1,31 @@
 // Classes
 class Car {
+    constructor(options) {
+        this.title = options.title;
+    }
+
     drive() {
         return 'vroom';
     }
+
+    honk() {
+        return 'bam';
+    }
 }
 
-let car = new Car();
-console.log(car.drive());
+class Honda extends Car {
+    constructor(options) {
+        super(options); // call Car.constructor()
+        this.color = options.color;
+    }
+    honk() {
+        // super(options); // call Car.honk()
+        return 'beep';
+    }
+}
+
+let honda = new Honda({ title: 'my car', color: 'red' });
+
+// let car = new Car({ title: 'Honda' });
+console.log(honda);
+// console.log(car.drive());
